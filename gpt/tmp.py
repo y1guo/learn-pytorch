0ca27aca2772ec0a625999f516e5b58ba751bfa9
y1guo <guoyi0328@gmail.com>
2023-03-12 16:25:12 -0700

    optimizations

--- gpt/tmp.py
+++ gpt/tmp.py
@@ -3,7 +3,6 @@ import gc
 import IPython.display as ipd
 import gradio as gr
 import numpy as np
-import re
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from TTS.api import TTS
 
@@ -60,17 +59,16 @@ tts = TTS(
     model_name="tts_models/en/vctk/vits", gpu=True
 )  # speaker: p243, p259, p263, p270, p306
 
-example_few_shots = [
-    """Yi: Hi, Pimon. Long time no see! How are you? My name is Yi. Do you still remember me?
-Pimon: It's you, Yi! It's so good to see you again! I haven't seen you for a while. What are you doing?
-Yi: Yeah, you are right, Pimon. I've been really busy working on the TA stuff. You know, I'm a teaching assistent. It took me two whole days to grade the quiz and homeowrks. I really wish I could get some sleep tonight.
-Pimon: Oh...Poor you. So sorry to hear that. I'm sure you'll get better. After all, the work are all done, aren't they?
-Yi: Yeah, probably. Thank you. I'm feeling better now.
-Pimon: No problem. Oh, by the way, I'm glad that you still remember my name. I mean, Pimon is not a common name.
-Yi: Of course I do. Don't you remember the days we travelled together? We were best friends.
-Pimon: Oh, you're so sweet. We'll always be best friends. 
-""",
-    """Yi: My name is Yi.
+"""<user>: Hi, Pimon. Long time no see! How are you? My name is Yi. Do you still remember me?
+<bot>: It's you, Yi! It's so good to see you again! I haven't seen you for a while. What are you doing?
+<user>: Yeah, you are right, Pimon. I've been really busy working on the TA stuff. You know, I'm a teaching assistent. It took me two whole days to grade the quiz and homeowrks. I really wish I could get some sleep tonight.
+<bot>: Oh...Poor you. So sorry to hear that. I'm sure you'll get better. After all, the work are all done, aren't they?
+<user>: Yeah, probably. Thank you. I'm feeling better now.
+<bot>: No problem. Oh, by the way, I'm glad that you still remember my name. I mean, Pimon is not a common name.
+<user>: Of course I do. Don't you remember the days we travelled together? We were best friends.
+<bot>: Oh, you're so sweet. We'll always be best friends. 
+"""
+"""Yi: My name is Yi.
 Pimon: My name is Pimon
 Yi: Your name is Pimon.
 Pimon: Your name is Yi.
@@ -80,8 +78,7 @@ Yi: Your name is Pimon.
 Pimon: Yes, my name is Pimon.
 Yi: Your name is Jane.
 Pimon: No, my name is not Jane. My name is Pimon.
-""",
-]
+"""
 
 chat_input_sample = """Do you think I'm annoying?"""
 
@@ -99,72 +96,37 @@ def chat(
     user_name,
     bot_name,
 ):
-    # prepare prompt including few-shot and chat history
-    # regulate each term every iteration since there might be '<br>' added into them.
-    chat_history_list = [
-        (chat_regulator(user_text), chat_regulator(bot_text))
-        for (user_text, bot_text) in chat_history_list
-    ]
-    chat_history_list += [[chat_regulator(chat_input), ""]]
-    chat_history = "\n".join(
-        [
-            f"{user_name}: {user_text}\n{bot_name}: {bot_text}"
-            for (user_text, bot_text) in chat_history_list
-        ]
+    if not chat_input:
+        chat_input = chat_input_sample
+    chat_history = ''.join([f"{user_name}: {user_text}\n{bot_name}: {bot_text}\n" for (user_text, bot_text) in chat_history_list])
+    chat_history += (
+        f"{user_name}: {chat_input}\n{bot_name}:"
     )
-    prompt = few_shot_training_prompt.strip() + "\n" + chat_history.strip()
+    prompt = few_shot_training_prompt + chat_history
 
-    # extract AI response
-    try:
-        # 10 trials of searching for the stop token, i.e., user_name+":". If not found, continue generating text.
-        recur_prompt = prompt
-        for _ in range(10):
-            encoded_input = tokenizer(recur_prompt, return_tensors="pt")
-            output_sequences = model.generate(
-                input_ids=encoded_input["input_ids"].cuda(),
-                max_new_tokens=max_new_tokens,
-                do_sample=do_sample,
-                temperature=temperature,
-                top_k=top_k,
-                top_p=top_p,
-                eos_token_id=int(eos_token_id),
-            )
-            generated_text = tokenizer.decode(output_sequences[0], skip_special_tokens=True)
-            eos_idx = generated_text.find(user_name + ":", len(prompt))
-            if eos_idx < 0:
-                recur_prompt = generated_text
-            else:
-                break
-    except:
-        generated_text = prompt + "[System Error]: Failed to do inference."
-        eos_idx = -1
-        print("[System Error]: Failed to do inference.")
+    encoded_input = tokenizer(prompt, return_tensors="pt")
+    output_sequences = model.generate(
+        input_ids=encoded_input["input_ids"].cuda(),
+        max_new_tokens=max_new_tokens,
+        do_sample=do_sample,
+        temperature=temperature,
+        top_k=top_k,
+        top_p=top_p,
+        eos_token_id=int(eos_token_id),
+    )
+    generated_text = tokenizer.decode(output_sequences[0], skip_special_tokens=True)
 
-    chat_response = generated_text[len(prompt) : eos_idx].replace(bot_name + ":", "")
-    regulated_response = chat_regulator(chat_response)
-    chat_history_list[-1][1] = regulated_response
+    chat_response = generated_text[len(prompt) :]
+    eos_idx = min(chat_response.find(user_name + ":"), chat_response.find(bot_name + ":"))
+    chat_response = chat_response[:eos_idx]
+
+    chat_history_list.append((chat_input, chat_response))
 
     # output to .wav using tts
-    try:
-        wav = tts.tts(text=regulated_response, speaker="p306")
-    except:
-        wav = [0 for _ in range(20000)]
-        print(f"[System Error]: Failed to generate audio for '{regulated_response}'.")
+    wav = tts.tts(text=chat_response, speaker="p306")
     wav_int16 = (np.array(wav) * 32767).astype(np.int16)
 
-    return (
-        chat_history_list,
-        "",
-        (22050, wav_int16),
-        str([generated_text]) + "\n" + str(chat_history_list),
-    )
-
-
-def chat_regulator(text):
-    if not text:
-        return "Silence..."
-    # return " ".join(re.split(" |\t|\n|\r", text)).strip()
-    return text.strip().replace("<br>", "")
+    return chat_history_list, (22050, wav_int16), ''
 
 
 with gr.Blocks() as demo:
@@ -178,7 +140,7 @@ with gr.Blocks() as demo:
                     "EleutherAI/gpt-neo-2.7B",
                     "EleutherAI/gpt-j-6B",
                 ],
-                value="EleutherAI/gpt-neo-2.7B",
+                value="EleutherAI/gpt-neo-125M",
                 label="GPT model",
             )
             gpt_dtype_radio = gr.Radio(
@@ -195,30 +157,26 @@ with gr.Blocks() as demo:
             user_name_box = gr.Textbox("Yi", label="User name")
             bot_name_box = gr.Textbox("Pimon", label="AI name")
         with gr.Column(scale=3):
-            with gr.Accordion("Few shot traning prompt"):
-                few_shot_training_prompt_box = gr.Textbox(show_label=False)
+            few_shot_training_prompt_box = gr.Textbox(
+                lines=20,
+                label="Few shot training prompt",
+            )
             clear_history_button = gr.Button("Clear History")
             chat_history_box = gr.Chatbot(label="History")
             chat_input_box = gr.Textbox(
-                show_label=False, placeholder="Do you think I'm annoying?"
+                label="You said", lines=2, placeholder="Do you think I'm annoying?"
             )
-            with gr.Accordion("Debug", open=False):
-                debug_info_box = gr.Textbox(show_label=False)
+            chat_button = gr.Button("Submit")
         with gr.Column(scale=1):
             chat_response_audio = gr.Audio()
-            example_few_shots_box = gr.Examples(
-                example_few_shots, few_shot_training_prompt_box
-            )
 
     gpt_load_button.click(
         load_gpt, inputs=[gpt_name_radio, gpt_dtype_radio], outputs=gpt_in_use_box
     )
-    clear_history_button.click(
-        lambda: ([], []), None, [chat_history_box, chat_history_box]
-    )
-    chat_input_box.submit(
+    clear_history_button.click(lambda: [], outputs=chat_history_box)
+    chat_button.click(
         chat,
-        [
+        inputs=[
             few_shot_training_prompt_box,
             chat_history_box,
             chat_input_box,
@@ -231,11 +189,10 @@ with gr.Blocks() as demo:
             user_name_box,
             bot_name_box,
         ],
-        [
+        outputs=[
             chat_history_box,
-            chat_input_box,
             chat_response_audio,
-            debug_info_box,
+            chat_input_box,
         ],
     )
 
